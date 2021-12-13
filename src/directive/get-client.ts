39768dc9bbbdaab74f57a9b3ad917f3e7d2e39e6
JCQuintas <juniorquintas@gmail.com>
2021-12-14 00:36:44 +0100

    chore: fix eslint in directives

--- src/directive/get-client.ts
+++ src/directive/get-client.ts
@@ -6,7 +6,9 @@ export const getClient = async (vnode: VNode, key: Input['key']) => {
   const client = vnode.context?.$splitIO.client(key)
 
   if (!client)
-    throw new VueSplitIOError(`Couldn't start a new client with key ${key}`)
+    throw new VueSplitIOError(
+      `Couldn't start a new client with key ${key || ''}`,
+    )
 
   await client?.ready()
   return client
--- src/directive/update-listener.ts
+++ src/directive/update-listener.ts
@@ -8,8 +8,14 @@ export const updateListener = (
   if (!vnode.context) return null
 
   if (!remove) {
-    client.addListener(client.Event.SDK_UPDATE, vnode.context.$forceUpdate)
+    client.addListener(
+      client.Event.SDK_UPDATE,
+      vnode.context.$forceUpdate.bind(vnode.context),
+    )
   } else {
-    client.removeListener(client.Event.SDK_UPDATE, vnode.context.$forceUpdate)
+    client.removeListener(
+      client.Event.SDK_UPDATE,
+      vnode.context.$forceUpdate.bind(vnode.context),
+    )
   }
 }
