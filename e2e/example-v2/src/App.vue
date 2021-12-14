5b927b1e9e9782f0e56d9a157c7f1118e52e93a6
JCQuintas <juniorquintas@gmail.com>
2021-12-14 23:15:46 +0100

    test: add example for testing

---
+++ e2e/example-v2/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">This is Vue SplitIO Feature Flag</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App',
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
